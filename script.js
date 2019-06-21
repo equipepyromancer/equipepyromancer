03a07b85612dfd769ca3e0a94ab5bed21becf5bf
equipepyromancer <52037700+equipepyromancer@users.noreply.github.com>
2019-06-21 04:52:00 -0300

    Muita coisa

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 function MudarTamanhoBaseFonte(e) {
+	//Acessa a raiz do CSS e pega a variável global com tamanho base das fontes redimensionáveis
 	var elemento = $(":root");
 	var fontSize = parseInt(elemento.css("--base-font-size"));
-  
-	if (e == 'a' && fontSize < 32) {
+
+	//Muda o tamanho da fonte dentro de um limite de acordo com o botão pressionado
+	if (e == 'a' && fontSize < 24) {
 		fontSize = fontSize + 2;
 	} else if (e == 'd' && fontSize > 10) { 
 		fontSize = fontSize - 2;
@@ -11,5 +13,16 @@ function MudarTamanhoBaseFonte(e) {
 		fontSize = 16;
 	}
 
+	//Aplica o valor alterado na variável global do CSS	
 	elemento.css("--base-font-size", fontSize);
+}
+
+function FormularioEnviado() {
+	//Alerta que o conteúdo do formulário foi enviado
+	alert("Formulário Enviado");
+
+	//Apaga o conteúdo do formulário
+	document.getElementById("email").value = "";
+	document.getElementById("assunto").value = "";
+	document.getElementById("mensagem").value = "";
 }
